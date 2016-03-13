c20976efd867b2d7a69d62c395bacd000a399993
Pavlina <pajuska369@gmail.com>
2016-03-13 12:41:32 +0100

    My first version

---
+++ prvy.py
@@ -0,0 +1,21 @@
+def x(a,b):
+    return a+b
+a=5
+if a>=0 and a<=5:
+    print("0 až 5")
+elif a>5 and a<=10:
+    print("5 až 10")
+else:
+    print("ani jedno")
+    
+print("koniec")
+
+print(x(5,6))
+
+x=[5,8,7,9,6,8,5,1,2,3,4]
+for i in x:
+    print(i)
+
+
+
+
