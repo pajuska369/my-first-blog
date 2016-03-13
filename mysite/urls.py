9522a55023588d9cc079595838741bdd354152f3
Pavlina <pajuska369@gmail.com>
2016-03-13 17:26:01 +0100

    Zmena niecoho

--- mysite/urls.py
+++ mysite/urls.py
@@ -19,4 +19,6 @@ from blog import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.post_list),
+    url(r'^post/(?P<pk>[0-9]+)$', views.post_detail, name='post_detail'),
+    url(r'^post/new$', views.post_new, name='post_new'),
 ]
